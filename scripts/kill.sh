69ae3677c403c4551fccfb59862691adb9a75d43
Siddharth-Yedlapati <siddharth.yedlapati@gmail.com>
2023-11-02 20:34:54 +0530

    Updated scripts

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -3,5 +3,5 @@
 echo 'This script stops the docker container in which we were running our app,'
 echo 'thereby shutting down our calculator application running on localhost:3000'
 set -x
-/usr/local/bin/docker stop $(cat ~/container_id.txt)
+/usr/bin/docker stop $(cat ~/container_id.txt)
 rm ~/container_id.txt
