cd991ee13e22a5a4bef2853bd548c9795d0d20d6
Siddharth-Yedlapati <siddharth.yedlapati@gmail.com>
2023-11-09 03:23:22 +0530

    Updated unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Calculator/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Result', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Result/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders equal button', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/=/i);
   expect(linkElement).toBeInTheDocument();
 });
